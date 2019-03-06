a5934b189c433fe272050e7368bceb4e1ce83e35
lgt8879 <lgt8879@gmail.com>
2019-03-06 17:03:15 +0900

    오라클 DB java

---
+++ src/osf/Execute.java
@@ -0,0 +1,11 @@
+package osf;
+
+import java.sql.Connection;
+
+import osf.db.DBConnector;
+
+public class Execute {
+	public static void main(String[] args) {
+		Connection con = DBConnector.getCon();
+	}
+}
---
+++ src/osf/db/DBConnector.java
@@ -0,0 +1,44 @@
+package osf.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+	private static final String URL = "jdbc:orcle:thin:@localhost:1521/xe";
+	private static final String USER = "osfu";
+	private static final String PASSWORD = "12345678";
+	private static Connection con = null;
+	static {
+		openCon();
+	}
+
+	private static void openCon() {
+		try {
+			con = DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getCon() {
+		if (con == null) {
+			openCon();
+		}
+		return con;
+	}
+
+	public static void closd() {
+		if (con != null) {
+			try {
+				if (con.isClosed()) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		con = null;
+	}
+
+}
